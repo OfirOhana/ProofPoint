8b5d62832f349e65859a4837c837fdeeafa6538f
unknown <ofirohana30@gmail.com>
2021-07-23 12:08:22 +0300

    first commit

---
+++ converter/src/converter/converter.py
@@ -0,0 +1,20 @@
+
+import json
+
+#Login into youtube
+def youtube_login():
+
+
+# Take the liked playlist
+def get_youtube_playlist():
+
+
+# Create a new playtlist
+def create_spotify_palylist():
+    reqest_body = json.dump {
+# Search for the song
+def get_uri():
+
+
+# Add this song into the new playlist in Spotify
+def add_song():
